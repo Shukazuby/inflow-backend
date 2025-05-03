a4b46bb28a7bc2a75753dbeb51d75fe420aa1d9c
Shukazuby <shukazuby@gmail.com>
2025-05-03 23:17:31 +0100

    Bug: added JwtService as wallet provider

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'nestjs-prisma';
 import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
     PrismaModule,
   ],
   controllers: [WalletController],
-  providers: [WalletService]
+  providers: [WalletService, JwtService]
 })
 export class WalletModule { }
