fb829166a7d6aa3ccb91ab741e18cd1dda176955
Rhoda Eboreime <rhodaivioje@gmail.com>
2025-05-22 15:14:59 +0100

    feat: implement posts endpoint for feed retrieval

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = parseInt(process.env.PORT, 10) || 3005;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('InFlow backend')
